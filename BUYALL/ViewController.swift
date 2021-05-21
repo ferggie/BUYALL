588c786ecb6dcaab194869c0b09aff5471c58e34
Fernando <fgutierrez1191@gmail.com>
2021-05-21 10:36:08 -0400

    Initial Commit

---
+++ BUYALL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BUYALL
+//
+//  Created by user191023 on 5/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
